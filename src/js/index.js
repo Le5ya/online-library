0eb5cd896153a05cfabcd0cba92fe3cba649b246
Le6ya <liliatolstyh@gmail.com>
2022-05-09 17:42:26 +0300

    buttons

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,14 @@ import Navigo from 'navigo';
 const library = document.querySelector('.library');
 const book = document.querySelector('.book');
 const add = document.querySelector('.add');
-const addBtns = document.querySelectorAll('.header__btn-add, .library__add-btn');
-const backBtns = document.querySelector('.header__btn-back');
 
-const router = new Navigo({
+const addBtns = document.querySelectorAll('.header__btn-add, .library__add-btn');
+const backBtns = document.querySelectorAll('.header__btn-back');
+const btnSearch = document.querySelectorAll('.header__btn-search');
+const search = document.querySelectorAll('.search');
+
+
+const router = new Navigo('/', {
     hash: true,
 });
 
@@ -31,8 +35,34 @@ router.on({
     }
 }).resolve();
 
-addBtns.forEach(btn => {
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = Array.prototype.forEach;
+ }
+
+ addBtns.forEach((btn) => {
     btn.addEventListener('click', () => {
         router.navigate('add');
     })
 })
+ backBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        router.navigate('/');
+    })
+});
+
+const closeSearch = ({target}) => {
+    if(target.closest('.search, .header__btn-search')) {
+        return;
+    }
+    search.classList.remove('search_active');
+}
+
+btnSearch.forEach(btn => {
+    btn.addEventListener('click', () => {
+        search.classList.add('search_active');
+        document.addEventListener('click', closeSearch);
+    })
+})
+
+ 
+
