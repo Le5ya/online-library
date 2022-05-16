823e749317c093e4bd52e5bc87e1dca1a5a5efcc
Le5ya <liliatolstyh@yandex.ru>
2022-05-16 22:53:53 +0300

    5 day 1hour

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,10 @@ const addBtns = document.querySelectorAll('.header__btn-add, .library__add-btn')
 const backBtns = document.querySelectorAll('.header__btn-back');
 const btnSearch = document.querySelectorAll('.header__btn-search');
 const search = document.querySelector('.search');
+const fieldsBtnSort = document.querySelector('.fields__btn_sort');
+const fieldsListSort = document.querySelector('.fields__list_sort');
+const fieldsBtnFilter = document.querySelector('.fields__btn_filter');
+const fieldsListFilter = document.querySelector('.fields__list_filter');
 
 const router = new Navigo('/', {
     hash: true,
@@ -16,40 +20,43 @@ const router = new Navigo('/', {
 const closeAllPage = () => {
     library.classList.add('hidden');
     book.classList.add('hidden');
-    add.classList.add('hidden');  
+    add.classList.add('hidden');
 }
 router.on({
     '/': () => {
         closeAllPage();
         library.classList.remove('hidden');
+        document.body.classList.remove('body_gradient');
     },
     'book': () => {
         closeAllPage();
         book.classList.remove('hidden');
+        document.body.classList.add('body_gradient');
     },
-    'add' : () => {
+    'add': () => {
         closeAllPage();
         add.classList.remove('hidden');
+        document.body.classList.add('body_gradient');
     }
 }).resolve();
 
 if (window.NodeList && !NodeList.prototype.forEach) {
     NodeList.prototype.forEach = Array.prototype.forEach;
- }
+}
 
- addBtns.forEach((btn) => {
+addBtns.forEach((btn) => {
     btn.addEventListener('click', () => {
         router.navigate('add');
     })
 })
- backBtns.forEach((btn) => {
+backBtns.forEach((btn) => {
     btn.addEventListener('click', () => {
         router.navigate('/');
     })
 });
 
-const closeSearch = ({target}) => {
-    if(target.closest('.search, .header__btn-search')) {
+const closeSearch = ({ target }) => {
+    if (target.closest('.search, .header__btn-search')) {
         return;
     }
     search.classList.remove('search_active');
@@ -58,9 +65,24 @@ const closeSearch = ({target}) => {
 btnSearch.forEach(btn => {
     btn.addEventListener('click', () => {
         search.classList.add('search_active');
-        document.addEventListener('click', closeSearch);
-    })
-})
+        document.body.addEventListener('click', closeSearch, true);
+    });
+});
 
- 
+const controlField = (btn, list) => {
+    btn.addEventListener('click', () => {
+        list.classList.toggle('fields__list_active');
+    });
+    list.addEventListener('click', ({ target }) => {
+        if (target.classList.contains('fields__button')) {
+            list.classList.remove('fields__list_active')
+        }
+    });
+};
+controlField(fieldsBtnSort, fieldsListSort);
+controlField(fieldsBtnFilter, fieldsListFilter);
+
+
+
+
 
