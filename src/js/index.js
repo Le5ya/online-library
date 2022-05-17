a619683b6a50da5667feabfe39e4b78aef0f5587
Le5ya <liliatolstyh@yandex.ru>
2022-05-17 22:01:39 +0300

    day-5 hour-2.12

--- src/js/index.js
+++ src/js/index.js
@@ -83,6 +83,44 @@ controlField(fieldsBtnSort, fieldsListSort);
 controlField(fieldsBtnFilter, fieldsListFilter);
 
 
+const changeFieldset = () => {
+    const fieldsets = document.querySelectorAll('.add__fieldset');
+    const addBtn = document.querySelector('.add__btn');
+    const form = document.querySelector('.add__form');
+
+    let count = 0;
+
+    addBtn.addEventListener('click', ({ target }) => {
+        const fieldset = fieldsets[count];
+
+        let valid = true;
+
+        for (const elem of fieldset.elements) {
+            if (!elem.checkValidity()) {
+                elem.classList.add('no-valid');
+                valid = false;
+            } else {
+                elem.classList.remove('no-valid');
+            }
+        }
+        if (valid) {
+            count += 1;
+
+            if (count === fieldsets.length - 1) {
+                addBtn.textContent = 'Добавить книгу';
+            }
+            if (count === fieldsets.length) {
+                form.reset();
+            }
+            fieldset.classList.add('hidden');
+            fieldsets[count].classList.remove('hidden');
+        }
+
+    })
+}
+changeFieldset();
+
+
 
 
 
