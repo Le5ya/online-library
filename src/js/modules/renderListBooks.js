7b9c43c30f9263050379661b3c74eb6c94ed2e21
Le6ya <liliatolstyh@gmail.com>
2022-06-06 17:10:00 +0300

    day-6 end

--- src/js/modules/renderListBooks.js
+++ src/js/modules/renderListBooks.js
@@ -2,7 +2,8 @@ import { getLabels, getBooks, API_URI } from "./serviceBook.js";
 
 const libraryList = document.querySelector('.library__list');
 
-const getStars = (raiting) => {
+
+const getStars = (rating) => {
 	const stars = [];
 	for (let i = 0; i < 5; i++) {
 		if (i === 0) {
@@ -10,40 +11,73 @@ const getStars = (raiting) => {
 		} else if (i < rating) {
 			stars.push(`<img class="star cart__rating-star" src="./img/star.svg" alt="">`);
 		} else {
-			satrs.push(`<img class="star cart__rating-star" src="./img/star-o.svg" alt="">`);
+			stars.push(`<img class="star cart__rating-star" src="./img/star-o.svg" alt="">`);
 		}
 	}
 	return stars;
 };
 
+// export const renderListBooks = async () => {
+
+// 	const [books, labels] = await Promise.all([getBooks(), getLabels()]);
+// 	libraryList.textContent = '';
+
+// 	books.forEach(({ author, description, id, image, label, rating, title }) => {
+// 		const item = document.createElement('li');
+// 		item.classList.add('library__item');
+// 		item.innerHTML = `
+// 		<a href="/#/book?id=${data.id}">
+// 						<article class="cart">
+// 							<div class="cart__wrapper">
+// 								<img src="${API_URI}${image}" alt="${title}" class="cart__img" />
+// 								<p class="cart__label">${labels[label]}</p>
+// 							</div>
+// 							<div class="cart__content">
+// 								<h3 class="cart__title">${title}</h3>
+// 								<p class="cart__author">${author}</p>
+// 								<p class="cart__description">
+// 									${description.substring(0, 80)}...
+// 								</p>
+// 								<div class="cart__rating">
+// 									${getStars(rating).join('')}
+// 								</div>
+// 							</div>
+// 						</article>
+// 					</a>
+// 		`;
+// 		libraryList.append(item);
+// 	});
+// };
+
 export const renderListBooks = async () => {
-
 	const [books, labels] = await Promise.all([getBooks(), getLabels()]);
-	libraryList.textContent = '';
 
-	books.forEach(({ author, description, id, image, label, rating, title }) => {
+	libraryList.textContent = '';
+		
+	books.forEach(({author, description, id, image, label, rating, title}) => {
 		const item = document.createElement('li');
 		item.classList.add('library__item');
 		item.innerHTML = `
-		<a href="/#/book?id=${data.id}">
+ 		<a href="/#/book?id=${id}">
 						<article class="cart">
-							<div class="cart__wrapper">
+ 							<div class="cart__wrapper"> 
 								<img src="${API_URI}${image}" alt="${title}" class="cart__img" />
 								<p class="cart__label">${labels[label]}</p>
-							</div>
-							<div class="cart__content">
+							</div> 							<div class="cart__content">
 								<h3 class="cart__title">${title}</h3>
-								<p class="cart__author">${author}</p>
+ 								<p class="cart__author">${author}</p>
 								<p class="cart__description">
-									${description.substring(0, 80)}...
-								</p>
+ 									${description.substring(0, 80)}... 	</p>
 								<div class="cart__rating">
-									${getStars(rating).join('')}
-								</div>
-							</div>
-						</article>
-					</a>
+ 									${getStars(rating).join('')}
+ 								</div>
+ 							</div>
+ 						</article>
+ 					</a>
+		
 		`;
 		libraryList.append(item);
+
 	});
+	
 };
--- src/js/modules/serviceBook.js
+++ src/js/modules/serviceBook.js
@@ -1,16 +1,20 @@
 export const API_URI = 'http://localhost:3024/';
 
-export const getBooks = async (id) => {
+
+export const getBooks =  async (id) => {
+
 	const response = await fetch(`${API_URI}api/books/`);
 	if (response.ok) {
 		return await response.json();
 	}
 	throw new Error(response.statusText);
+	// console.log(await response.json());
 }
 export const getLabels = async () => {
-	const response = await fetch(`${API_URI}label/`);
+	const response = await fetch(`${API_URI}api/label/`);
 	if (response.ok) {
 		return await response.json();
 	}
 	throw new Error(response.statusText);
+	//  console.log(await response.json);
 }
